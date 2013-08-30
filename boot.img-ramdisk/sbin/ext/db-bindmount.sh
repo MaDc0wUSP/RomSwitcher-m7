35df2c8903c41b9f193c1505f234507d6babc72b
Grarak <williye97@gmail.com>
2013-08-30 19:58:24 +0200

    dualboot ramdisk
    Signed-off-by: Grarak <williye97@gmail.com>

---
+++ boot.img-ramdisk/sbin/ext/db-bindmount.sh
@@ -0,0 +1,15 @@
+#!/sbin/busybox sh
+
+mkdir -p /1stdata/dual/2nddata
+mount --bind /1stdata/dual/2nddata /data
+
+mount -o remount,rw /system
+/sbin/busybox mount -t rootfs -o remount,rw rootfs
+mount -t tmpfs tmpfs /system/lib/modules
+
+chmod 771 /1stdata
+chmod 755 /system
+ln -s /lib/modules/* /system/lib/modules/
+
+/sbin/busybox mount -t rootfs -o remount,ro rootfs
+mount -o remount,ro /system
