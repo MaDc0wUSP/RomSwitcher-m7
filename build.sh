2762e0ae447bfd102df91fab7cfa071bdc97f605
Grarak <williye97@gmail.com>
2013-08-30 20:06:36 +0200

    correct ramdisk offset
    Signed-off-by: Grarak <williye97@gmail.com>

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ find -name "*~" -exec rm -rf {} \;
 cd boot.img-ramdisk
 find . | cpio -o -H newc | gzip -9 > ../ramdisk.gz
 cd ..
-./mkbootimg --kernel zImage --ramdisk ramdisk.gz --base 0x80600000 --ramdisk_offset 0x02000000 --cmdline "console=ttyHSL0,115200,n8 androidboot.hardware=qcom user_debug=31" -o boot.img
+./mkbootimg --kernel zImage --ramdisk ramdisk.gz --base 0x80600000 --ramdisk_offset 0x01400000 --cmdline "console=ttyHSL0,115200,n8 androidboot.hardware=qcom user_debug=31" -o boot.img
 
 mv -v boot.img out/
 cd out
