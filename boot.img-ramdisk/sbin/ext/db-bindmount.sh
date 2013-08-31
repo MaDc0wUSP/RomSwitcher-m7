75837af217305a6da432a878a842c2a2a079b4e3
Grarak <williye97@gmail.com>
2013-08-31 12:47:14 +0200

    remove system mount points
    Signed-off-by: Grarak <williye97@gmail.com>

--- boot.img-ramdisk/sbin/ext/db-bindmount.sh
+++ boot.img-ramdisk/sbin/ext/db-bindmount.sh
@@ -3,7 +3,6 @@
 mkdir -p /1stdata/dual/2nddata
 mount --bind /1stdata/dual/2nddata /data
 
-mount -o remount,rw /system
 /sbin/busybox mount -t rootfs -o remount,rw rootfs
 mount -t tmpfs tmpfs /system/lib/modules
 
@@ -11,4 +10,3 @@ chmod 755 /system
 ln -s /lib/modules/* /system/lib/modules/
 
 /sbin/busybox mount -t rootfs -o remount,ro rootfs
-mount -o remount,ro /system
