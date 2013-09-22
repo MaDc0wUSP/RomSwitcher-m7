9f6200ef68f608c0caf8d39d7e37b12a9dececa8
MaDc0wUSP <MaDc0w@undergroundz.org>
2013-09-22 14:40:27 +0200

    Update mkbootimg

--- boot.img-ramdisk/sbin/ext/db-bindmount.sh
+++ boot.img-ramdisk/sbin/ext/db-bindmount.sh
@@ -6,11 +6,28 @@ mount --bind /1stdata/app /data/app
 mount --bind /1stdata/app-asec /data/app-asec
 mount --bind /1stdata/misc/systemkeys /data/misc/systemkeys
 
-/sbin/busybox mount -t rootfs -o remount,rw rootfs
+mount -t rootfs -o remount,rw rootfs
 mount -t tmpfs tmpfs /system/lib/modules
 
 chmod 771 /1stdata
 chmod 755 /system
 ln -s /lib/modules/* /system/lib/modules/
 
-/sbin/busybox mount -t rootfs -o remount,ro rootfs
+mount -t rootfs -o remount,ro rootfs
+
+# n3ocort3x's teaMseven kernel compatibility
+if uname -r | grep "teaMseven" > /dev/null ; then
+	mkdir -p /system/etc/init.d
+	chmod 755 /system/etc/init.d
+	cp -f /usr/keylayout/synaptics-rmi-touchscreen.kl /system/usr/keylayout/synaptics-rmi-touchscreen.kl
+	chmod 644 /system/usr/keylayout/synaptics-rmi-touchscreen.kl
+
+	if [ -e /system/bin/mpdecision ] ; then
+		mv /system/bin/mpdecision /system/bin/mpdecision_bck
+	fi
+
+elif uname -r | grep "CM" > /dev/null ; then
+	if [ -e /system/bin/mpdecision_bck ] ; then
+		mv /system/bin/mpdecision_bck /system/bin/mpdecision
+	fi
+fi
--- build.sh
+++ build.sh
@@ -1,17 +1,20 @@
 #!/bin/sh
 
+echo "Cleanup unused pre build..."
 rm -rf kernel.zip
 rm -rf ramdisk.gz
 find -name "*~" -exec rm -rf {} \;
 
+echo "Building ramdisk and packaging kernel..."
 cd boot.img-ramdisk
 find . | cpio -o -H newc | gzip > ../ramdisk.gz
 cd ..
-./mkbootimg --kernel zImage --ramdisk ramdisk.gz --base 0x80600000 --ramdisk_offset 0x01400000 --pagesize 2048 --cmdline "console=ttyHSL0,115200,n8 androidboot.hardware=qcom user_debug=31" -o boot.img
+./mkbootimg --kernel zImage --ramdisk ramdisk.gz --base 0x80600000 --ramdiskaddr 0x82200000 --pagesize 2048 --cmdline "console=ttyHSL0,115200,n8 androidboot.hardware=qcom user_debug=31" -o boot.img
 
+echo "Building flashable kernel.zip and copying boot.img to romswitcher folder.."
 mv -v boot.img out/
 cd out
-adb push boot.img /sdcard/romswitcher/second.img
+adb push boot.img /storage/emulated/0/romswitcher/second.img
 zip -r kernel.zip META-INF boot.img
 mv -v kernel.zip ../
 cd ..
