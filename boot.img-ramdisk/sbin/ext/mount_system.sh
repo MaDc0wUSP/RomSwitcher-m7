d80e070133f9ee8b6ee758e524d21eaf2831cc70
MaDc0wUSP <MaDc0w@undergroundz.org>
2013-10-13 01:48:27 +0200

    revert rw

--- boot.img-ramdisk/sbin/ext/mount_system.sh
+++ boot.img-ramdisk/sbin/ext/mount_system.sh
@@ -6,8 +6,8 @@ mkdir -p /system
 
 if [ "$ROM" == "secondary" ]; then
     losetup /dev/block/loop0 /.firstrom/media/.secondrom/system.img
-    mount -t ext4 -o rw /.firstrom/media/.secondrom/system.img /system
+    mount -t ext4 -o ro /.firstrom/media/.secondrom/system.img /system
 elif [ "$ROM" == "tertiary" ]; then
     losetup /dev/block/loop0 /.firstrom/media/.thirdrom/system.img
-    mount -t ext4 -o rw /.firstrom/media/.thirdrom/system.img /system
+    mount -t ext4 -o ro /.firstrom/media/.thirdrom/system.img /system
 fi
