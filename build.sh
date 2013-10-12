59ea58aab98bf26e011bcf3b131077de86d762ad
Grarak <williye97@gmail.com>
2013-10-13 01:30:56 +0200

    smaller kernel
    Signed-off-by: Grarak <williye97@gmail.com>

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ echo "Building ramdisk and packaging kernel..."
 cd boot.img-ramdisk
 find . | cpio -o -H newc | gzip > ../ramdisk.gz
 cd ..
-./mkbootimg --kernel zImage --ramdisk ramdisk.gz --base 0x80600000 --ramdisk_offset 0x01400000 --pagesize 2048 --cmdline "console=ttyHSL0,115200,n8 androidboot.hardware=qcom user_debug=31" -o boot.img
+./mkbootimg --kernel zImage --ramdisk ramdisk.gz --base 0x80600000 --ramdisk_offset 0x01400000 --cmdline "console=ttyHSL0,115200,n8 androidboot.hardware=qcom user_debug=31" -o boot.img
 
 echo "Building flashable kernel.zip and copying boot.img to romswitcher folder.."
 adb push boot.img /storage/emulated/0/romswitcher/second.img
